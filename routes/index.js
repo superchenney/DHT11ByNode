a0c3b9237c437cd2999d92368e70dcf4a62d0f90
superchenney <superchenney@gmail.com>
2016-05-11 16:18:26 +0800

    e

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,12 @@ var WarningRecord = global.dbHandel.getModel('wr');
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: '首页' });
-
 });
 
 
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
 
 
 
