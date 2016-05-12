c283d0e0e2205c7a75bab6821091267bcb0c1513
superchenney <superchenney@gmail.com>
2016-05-12 19:38:03 +0800

    x

--- routes/index.js
+++ routes/index.js
@@ -168,13 +168,14 @@ router.post('/login', function(req, res, next) {
             console.log(err);
         } else if (!doc) {
             req.session.error = '手机号未注册';
-            res.send('error');
+            // res.send('error');
             console.log("用户" + req.body.loginphoneNum + "不存在");
         } else {
             if (doc.pwd != req.body.loginPwd) {
                 req.session.error = "密码输入错误";
                 // res.send(500);
-                res.send('error');
+                // res.send('error');
+                res.send(500);
                 console.log(req.body.loginphoneNum + "密码错误");
             } else {
                 // console.log("doc：" + doc);
